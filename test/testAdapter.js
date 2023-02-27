14909c091480b7ed998e13e1a8010baa6c76208f
GermanBluefox <dogafox@gmail.com>
2023-02-27 13:17:40 +0100

    Just update packages

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -1,19 +1,19 @@
 /* jshint -W097 */
 /* jshint strict:false */
 /* jslint node: true */
-var expect = require('chai').expect;
-var setup  = require(__dirname + '/lib/setup');
+const expect = require('chai').expect;
+const setup = require('@iobroker/legacy-testing');
 
-var objects = null;
-var states  = null;
-var onStateChanged = null;
-// var onObjectChanged = null;
-var sendToID = 1;
-var Server = require(__dirname + '/lib/s7Server.js');
-var server;
+let objects = null;
+let states  = null;
+let onStateChanged = null;
+// const onObjectChanged = null;
+let sendToID = 1;
+const Server = require('./lib/s7Server.js');
+let server;
 
-var adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.') + 1);
-var runningMode = require(__dirname + '/../io-package.json').common.mode;
+const adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.') + 1);
+const runningMode = require('../io-package.json').common.mode;
 
 function checkConnectionOfAdapter(cb, counter) {
     counter = counter || 0;
@@ -23,14 +23,12 @@ function checkConnectionOfAdapter(cb, counter) {
         return;
     }
 
-    states.getState('system.adapter.' + adapterShortName + '.0.alive', function (err, state) {
+    states.getState('system.adapter.' + adapterShortName + '.0.alive', (err, state) => {
         if (err) console.error(err);
         if (state && state.val) {
             if (cb) cb();
         } else {
-            setTimeout(function () {
-                checkConnectionOfAdapter(cb, counter + 1);
-            }, 1000);
+            setTimeout(() => checkConnectionOfAdapter(cb, counter + 1), 1000);
         }
     });
 }
@@ -77,12 +75,12 @@ function sendTo(target, command, message, callback) {
     });
 }
 
-describe('Test ' + adapterShortName + ' adapter', function() {
+describe('Test ' + adapterShortName + ' adapter', function () {
     before('Test ' + adapterShortName + ' adapter: Start js-controller', function (_done) {
         this.timeout(600000); // because of first install from npm
 
-        setup.setupController(async function () {
-            var config = await setup.getAdapterConfig();
+        setup.setupController(async () => {
+            const config = await setup.getAdapterConfig();
             // enable adapter
             config.common.enabled  = true;
             config.common.loglevel = 'debug';
@@ -158,15 +156,17 @@ describe('Test ' + adapterShortName + ' adapter', function() {
             await setup.setAdapterConfig(config.common, config.native);
             //if (/^win/.test(process.platform) || /^darwin/.test(process.platform)) {
             let bindIp = '127.0.0.1';
-            if (/^darwin/.test(process.platform)) bindIp = '0.0.0.0';
-                server = new Server();
-                server.start(bindIp);
+            if (/^darwin/.test(process.platform)) {
+                bindIp = '0.0.0.0';
+            }
+            server = new Server();
+            server.start(bindIp);
             //}
 
-            setup.startController(true, function(id, obj) {}, function (id, state) {
-                    if (onStateChanged) onStateChanged(id, state);
-                },
-                function (_objects, _states) {
+            setup.startController(true,
+                (/* id, obj */) => {},
+                (id, state) => onStateChanged && onStateChanged(id, state),
+                (_objects, _states) => {
                     objects = _objects;
                     states  = _states;
                     _done();
@@ -175,7 +175,7 @@ describe('Test ' + adapterShortName + ' adapter', function() {
     });
 
     it('Test ' + adapterShortName + ' instance object: it must exists', function (done) {
-        objects.getObject('system.adapter.' + adapterShortName + '.0', function (err, obj) {
+        objects.getObject('system.adapter.' + adapterShortName + '.0', (err, obj) => {
             expect(err).to.be.null;
             expect(obj).to.be.an('object');
             expect(obj).not.to.be.null;
@@ -185,8 +185,8 @@ describe('Test ' + adapterShortName + ' adapter', function() {
 
     it('Test ' + adapterShortName + ' adapter: Check if adapter started', function (done) {
         this.timeout(60000);
-        checkConnectionOfAdapter(function (res) {
-            if (res) console.log(res);
+        checkConnectionOfAdapter(res => {
+            res && console.log(res);
             if (runningMode === 'daemon') {
                 expect(res).not.to.be.equal('Cannot check connection');
             } else {
@@ -200,16 +200,16 @@ describe('Test ' + adapterShortName + ' adapter', function() {
         this.timeout(10000);
         // Linux required sudo for ports < 1000. S7 Server runs on TCP 102
         if (server) {
-            setTimeout(function () {
-                states.getState(adapterShortName + '.0.DBs.DB1.10', function (err, state) { //s7.0.DBs.DB1.10
+            setTimeout(() => {
+                states.getState(adapterShortName + '.0.DBs.DB1.10', (err, state) => { //s7.0.DBs.DB1.10
                     expect(state.val).to.be.equal(0x0A0B);
-                    states.getState(adapterShortName + '.0.DBs.DB1.12', function (err, state) { //s7.0.DBs.DB1.10
+                    states.getState(adapterShortName + '.0.DBs.DB1.12', (err, state) => { //s7.0.DBs.DB1.10
                         expect(state.val).to.be.equal(0x0C0D);
 
-                        states.getState(adapterShortName + '.0.DBs.DB1.14_0', function (err, state) { //s7.0.DBs.DB1.10
+                        states.getState(adapterShortName + '.0.DBs.DB1.14_0', (err, state) => { //s7.0.DBs.DB1.10
                             expect(state.val).to.be.equal(false);
 
-                            states.getState(adapterShortName + '.0.DBs.DB1.14_1', function (err, state) { //s7.0.DBs.DB1.10
+                            states.getState(adapterShortName + '.0.DBs.DB1.14_1', (err, state) => { //s7.0.DBs.DB1.10
                                 expect(state.val).to.be.equal(true);
                                 done();
                             });
@@ -237,7 +237,7 @@ describe('Test ' + adapterShortName + ' adapter', function() {
         this.timeout(10000);
         server && server.stop();
         server = null;
-        setup.stopController(function (normalTerminated) {
+        setup.stopController(normalTerminated => {
             console.log('Adapter normal terminated: ' + normalTerminated);
             done();
         });
