fc41b6731697c5a9783c483b6e2201526fda1431
GermanBluefox <dogafox@gmail.com>
2026-08-04 21:10:22 +0300

    Fixing CI

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -1,11 +1,11 @@
 /* jshint -W097 */
 /* jshint strict:false */
 /* jslint node: true */
-const expect = require('chai').expect;
+const assert = require('node:assert');
 const setup = require('@iobroker/legacy-testing');
 
 let objects = null;
-let states  = null;
+let states = null;
 let onStateChanged = null;
 // const onObjectChanged = null;
 let sendToID = 1;
@@ -16,17 +16,17 @@ const adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('
 const runningMode = require('../io-package.json').common.mode;
 
 function checkConnectionOfAdapter(cb, counter) {
-    counter = counter || 0;
-    console.log('Try check #' + counter);
+    counter ||= counter || 0;
+    console.log(`Try check #${counter}`);
     if (counter > 30) {
-        if (cb) cb('Cannot check connection');
+        cb?.('Cannot check connection');
         return;
     }
 
-    states.getState('system.adapter.' + adapterShortName + '.0.alive', (err, state) => {
+    states.getState(`system.adapter.${adapterShortName}.0.alive`, (err, state) => {
         if (err) console.error(err);
         if (state && state.val) {
-            if (cb) cb();
+            cb?.();
         } else {
             setTimeout(() => checkConnectionOfAdapter(cb, counter + 1), 1000);
         }
@@ -34,21 +34,20 @@ function checkConnectionOfAdapter(cb, counter) {
 }
 
 function checkValueOfState(id, value, cb, counter) {
-    counter = counter || 0;
+    counter ||= 0;
     if (counter > 20) {
-        if (cb) cb('Cannot check value Of State ' + id);
+        cb?.('Cannot check value Of State ' + id);
         return;
     }
 
     states.getState(id, function (err, state) {
         if (err) console.error(err);
         if (value === null && !state) {
-            if (cb) cb();
-        } else
-        if (state && (value === undefined || state.val === value)) {
-            if (cb) cb();
+            cb?.();
+        } else if (state && (value === undefined || state.val === value)) {
+            cb?.();
         } else {
-            setTimeout(function () {
+            setTimeout(() => {
                 checkValueOfState(id, value, cb, counter + 1);
             }, 500);
         }
@@ -56,100 +55,99 @@ function checkValueOfState(id, value, cb, counter) {
 }
 
 function sendTo(target, command, message, callback) {
-    onStateChanged = function (id, state) {
+    onStateChanged = (id, state) => {
         if (id === 'messagebox.system.adapter.test.0') {
             callback(state.message);
         }
     };
 
-    states.pushMessage('system.adapter.' + target, {
-        command:    command,
-        message:    message,
-        from:       'system.adapter.test.0',
+    states.pushMessage(`system.adapter.${target}`, {
+        command: command,
+        message: message,
+        from: 'system.adapter.test.0',
         callback: {
             message: message,
-            id:      sendToID++,
-            ack:     false,
-            time:    (new Date()).getTime()
-        }
+            id: sendToID++,
+            ack: false,
+            time: new Date().getTime(),
+        },
     });
 }
 
-describe('Test ' + adapterShortName + ' adapter', function () {
-    before('Test ' + adapterShortName + ' adapter: Start js-controller', function (_done) {
+describe(`Test ${adapterShortName} adapter`, function () {
+    before(`Test ${adapterShortName} adapter: Start js-controller`, function (_done) {
         this.timeout(600000); // because of first install from npm
 
         setup.setupController(async () => {
             const config = await setup.getAdapterConfig();
             // enable adapter
-            config.common.enabled  = true;
+            config.common.enabled = true;
             config.common.loglevel = 'debug';
             config.native.params = {
-                "ip": "127.0.0.1",
-                "rack": "0",
-                "slot": "2",
-                "localTSAP": null,
-                "remoteTSAP": null,
-                "round": "2",
-                "poll": "1000",
-                "recon": "60000",
-                "pulsetime": "1000"
+                ip: '127.0.0.1',
+                rack: '0',
+                slot: '2',
+                localTSAP: null,
+                remoteTSAP: null,
+                round: '2',
+                poll: '1000',
+                recon: '60000',
+                pulsetime: '1000',
             };
             config.native.dbs = [
                 {
-                    "Address": "DB1 10",
-                    "Name": "",
-                    "Description": "",
-                    "Type": "WORD",
-                    "Length": "",
-                    "Unit": "",
-                    "Role": "value",
-                    "Room": "",
-                    "poll": true,
-                    "RW": false,
-                    "WP": false
+                    Address: 'DB1 10',
+                    Name: '',
+                    Description: '',
+                    Type: 'WORD',
+                    Length: '',
+                    Unit: '',
+                    Role: 'value',
+                    Room: '',
+                    poll: true,
+                    RW: false,
+                    WP: false,
                 },
                 {
-                    "Address": "DB1 12",
-                    "Name": "",
-                    "Description": "",
-                    "Type": "WORD",
-                    "Length": "",
-                    "Unit": "",
-                    "Role": "",
-                    "Room": "",
-                    "poll": true,
-                    "RW": false,
-                    "WP": false
+                    Address: 'DB1 12',
+                    Name: '',
+                    Description: '',
+                    Type: 'WORD',
+                    Length: '',
+                    Unit: '',
+                    Role: '',
+                    Room: '',
+                    poll: true,
+                    RW: false,
+                    WP: false,
                 },
                 {
-                    "Address": "DB1 14.0",
-                    "Name": "",
-                    "Description": "",
-                    "Type": "BOOL",
-                    "Length": "",
-                    "Unit": "",
-                    "Role": "state",
-                    "Room": "",
-                    "poll": true,
-                    "RW": false,
-                    "WP": false
+                    Address: 'DB1 14.0',
+                    Name: '',
+                    Description: '',
+                    Type: 'BOOL',
+                    Length: '',
+                    Unit: '',
+                    Role: 'state',
+                    Room: '',
+                    poll: true,
+                    RW: false,
+                    WP: false,
                 },
                 {
-                    "Address": "DB1 14.1",
-                    "Name": "",
-                    "Description": "",
-                    "Type": "BOOL",
-                    "Length": "",
-                    "Unit": "",
-                    "Role": "",
-                    "Room": "",
-                    "poll": true,
-                    "RW": false,
-                    "WP": false
-                }
+                    Address: 'DB1 14.1',
+                    Name: '',
+                    Description: '',
+                    Type: 'BOOL',
+                    Length: '',
+                    Unit: '',
+                    Role: '',
+                    Room: '',
+                    poll: true,
+                    RW: false,
+                    WP: false,
+                },
             ];
-
 
             //config.native.dbtype   = 'sqlite';
 
@@ -163,32 +161,33 @@ describe('Test ' + adapterShortName + ' adapter', function () {
             server.start(bindIp);
             //}
 
-            setup.startController(true,
+            setup.startController(
+                true,
                 (/* id, obj */) => {},
                 (id, state) => onStateChanged && onStateChanged(id, state),
                 (_objects, _states) => {
                     objects = _objects;
-                    states  = _states;
+                    states = _states;
                     _done();
-                });
+                },
+            );
         });
     });
 
-    it('Test ' + adapterShortName + ' instance object: it must exists', function (done) {
-        objects.getObject('system.adapter.' + adapterShortName + '.0', (err, obj) => {
-            expect(err).to.be.null;
-            expect(obj).to.be.an('object');
-            expect(obj).not.to.be.null;
+    it(`Test ${adapterShortName} instance object: it must exists`, function (done) {
+        objects.getObject(`system.adapter.${adapterShortName}.0`, (err, obj) => {
+            assert.strictEqual(err, null);
+            assert.ok(obj && typeof obj === 'object' && !Array.isArray(obj), 'obj must be an object');
             done();
         });
     });
 
-    it('Test ' + adapterShortName + ' adapter: Check if adapter started', function (done) {
+    it(`Test ${adapterShortName} adapter: Check if adapter started`, function (done) {
         this.timeout(60000);
         checkConnectionOfAdapter(res => {
             res && console.log(res);
             if (runningMode === 'daemon') {
-                expect(res).not.to.be.equal('Cannot check connection');
+                assert.notStrictEqual(res, 'Cannot check connection');
             } else {
                 //??
             }
@@ -196,21 +195,25 @@ describe('Test ' + adapterShortName + ' adapter', function () {
         });
     });
 
-    it('Test ' + adapterShortName + ' adapter: Read words', function (done) {
+    it(`Test ${adapterShortName} adapter: Read words`, function (done) {
         this.timeout(10000);
         // Linux required sudo for ports < 1000. S7 Server runs on TCP 102
         if (server) {
             setTimeout(() => {
-                states.getState(adapterShortName + '.0.DBs.DB1.10', (err, state) => { //s7.0.DBs.DB1.10
-                    expect(state.val).to.be.equal(0x0A0B);
-                    states.getState(adapterShortName + '.0.DBs.DB1.12', (err, state) => { //s7.0.DBs.DB1.10
-                        expect(state.val).to.be.equal(0x0C0D);
+                states.getState(`${adapterShortName}.0.DBs.DB1.10`, (err, state) => {
+                    //s7.0.DBs.DB1.10
+                    assert.strictEqual(state.val, 0x0a0b);
+                    states.getState(`${adapterShortName}.0.DBs.DB1.12`, (err, state) => {
+                        //s7.0.DBs.DB1.10
+                        assert.strictEqual(state.val, 0x0c0d);
 
-                        states.getState(adapterShortName + '.0.DBs.DB1.14_0', (err, state) => { //s7.0.DBs.DB1.10
-                            expect(state.val).to.be.equal(false);
+                        states.getState(`${adapterShortName}.0.DBs.DB1.14_0`, (err, state) => {
+                            //s7.0.DBs.DB1.10
+                            assert.strictEqual(state.val, false);
 
-                            states.getState(adapterShortName + '.0.DBs.DB1.14_1', (err, state) => { //s7.0.DBs.DB1.10
-                                expect(state.val).to.be.equal(true);
+                            states.getState(`${adapterShortName}.0.DBs.DB1.14_1`, (err, state) => {
+                                //s7.0.DBs.DB1.10
+                                assert.strictEqual(state.val, true);
                                 done();
                             });
                         });
@@ -222,9 +225,9 @@ describe('Test ' + adapterShortName + ' adapter', function () {
         }
     });
 
-/**/
+    /**/
 
-/*
+    /*
     PUT YOUR OWN TESTS HERE USING
     it('Testname', function ( done) {
         ...
@@ -233,12 +236,12 @@ describe('Test ' + adapterShortName + ' adapter', function () {
     You can also use "sendTo" method to send messages to the started adapter
 */
 
-    after('Test ' + adapterShortName + ' adapter: Stop js-controller', function (done) {
+    after(`Test ${adapterShortName} adapter: Stop js-controller`, function (done) {
         this.timeout(10000);
         server && server.stop();
         server = null;
         setup.stopController(normalTerminated => {
-            console.log('Adapter normal terminated: ' + normalTerminated);
+            console.log(`Adapter normal terminated: ${normalTerminated}`);
             done();
         });
     });
